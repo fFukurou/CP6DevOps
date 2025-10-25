5e8d6dc308e6b6cb5f7d674c3eb4ccc06d45f815
fFukurou <supevi1@gmail.com>
2025-10-25 19:20:15 -0300

    added starter files

---
+++ src/main/java/br/com/greeks/greeks/GreeksApplication.java
@@ -0,0 +1,13 @@
+package br.com.greeks.greeks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreeksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreeksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/greeks/greeks/GreeksApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.greeks.greeks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreeksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
